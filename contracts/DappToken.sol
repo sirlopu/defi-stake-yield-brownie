205730e0c118f049959740f286ff589d608147d5
Gabriel S <sirlopug@gmail.com>
2022-03-27 15:48:01 -0700

    fixed token import

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.0;
 
-import "openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract DappToken is ERC20 {
     constructor() public ERC20("Dapp Token", "DAPP"){
