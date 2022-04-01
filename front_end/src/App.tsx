75c759ccedb8b3034140040a8cf73beba524bdd3
Gabriel S <sirlopug@gmail.com>
2022-03-31 23:08:45 -0700

    added wallet header connection

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Header } from "./components/Header"
 import { DAppProvider, ChainId } from '@usedapp/core';
 
 function App() {
@@ -6,7 +7,7 @@ function App() {
     <DAppProvider config={{
       supportedChains: [ChainId.Kovan, ChainId.Rinkeby]
     }}>
-      <div>Hi</div>
+      <Header />
     </DAppProvider>
   );
 }
