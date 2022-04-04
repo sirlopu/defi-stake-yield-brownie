3b37a447c53e6bef83d8cf49319fea557d8f4a4a
Gabriel S <sirlopug@gmail.com>
2022-04-03 22:22:06 -0700

    began wallet implementation

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -2,15 +2,17 @@ import React from 'react';
 import { Header } from "./components/Header"
 import { DAppProvider, ChainId } from '@usedapp/core';
 import { Container } from "@material-ui/core";
+import { Main } from './components/Main';
 
 function App() {
   return (
     <DAppProvider config={{
-      supportedChains: [ChainId.Kovan, ChainId.Rinkeby]
+      //supportedChains: [ChainId.Kovan, ChainId.Rinkeby]
+      supportedChains: [ChainId.Kovan]
     }}>
       <Header />
       <Container maxWidth="md">
-        <div>test</div>
+        <Main />
       </Container>
     </DAppProvider>
   );
--- front_end/src/components/Main.tsx
+++ front_end/src/components/Main.tsx
@@ -1,3 +1,18 @@
+import { useEthers } from "@usedapp/core"
+import helperConfig from "../helper-config.json"
+import networkMapping from "../chain-info/deployments/map.json"
+import { constants } from "ethers"
+import brownieConfig from "../brownie-config.json"
+import { YourWallet } from "./yourWallet"
+import { makeStyles } from "@material-ui/core"
+
+const useStyles = makeStyles((theme) => ({
+    title: {
+        color: theme.palette.common.white,
+        textAlign: "center",
+        padding: theme.spacing(4)
+    }
+}))
 
 export const Main = () => {
     // show token values from the wallet
@@ -8,4 +23,21 @@ export const Main = () => {
     // send the brownie-config to our `src` folder
 
     // send the build folder
+    const classes = useStyles()
+
+    const { chainId, error } = useEthers()
+    const networkName = chainId ? helperConfig[chainId] : "dev"
+
+    const dappTokenAddress = chainId ? networkMapping[String(chainId)]["DappToken"][0] : constants.AddressZero
+    const wethTokenAdress = chainId ? brownieConfig["networks"][networkName]["weth_token"] : constants.AddressZero
+    const fauTokenAddress = chainId ? brownieConfig["networks"][networkName]["fau_token"] : constants.AddressZero
+
+    // console.log(chainId)
+    // console.log(networkName)
+
+    return (<>
+        <h2 className={classes.title}>Dapp Token App</h2>
+        <YourWallet supportedTokens={supportedTokens} />
+    </>)
+
 }
