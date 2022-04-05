8adb83b0ea84ac34563957bc69e993291401d7c7
Gabriel S <sirlopug@gmail.com>
2022-04-04 22:01:23 -0700

    added global notification setting

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -8,7 +8,11 @@ function App() {
   return (
     <DAppProvider config={{
       //supportedChains: [ChainId.Kovan, ChainId.Rinkeby]
-      supportedChains: [ChainId.Kovan]
+      supportedChains: [ChainId.Kovan],
+      notifications: {
+        expirationPeriod: 1000,
+        checkInterval: 1000
+      }
     }}>
       <Header />
       <Container maxWidth="md">
